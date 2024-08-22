58b0232686b5c4348aab609c24dd56d09debc274
technowin <technowinitinfra@gmail.com>
2024-08-22 17:52:49 +0530

    fsdfdf

--- Account/models.py
+++ Account/models.py
@@ -11,9 +11,7 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        user.password_text = password
         user.set_password(password)
-        user.encrypted_password = user.password
         user.save(using=self._db)
         return user
 
@@ -24,29 +22,26 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    title = models.CharField(max_length=255)
     full_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    encrypted_password = models.CharField(max_length=128)  # Adjust the max_length as needed
-    phone = models.CharField(max_length=15,unique=True)  # Adjust the max_length as needed
+    phone = models.CharField(max_length=15, unique=True)  # Adjust the max_length as needed
     first_time_login = models.IntegerField(default=1)  # 1 for True, 0 for False
     last_login = models.DateTimeField(default=timezone.now)
-    password_text = models.CharField(max_length=128)  # Store unencrypted password (not recommended)
 
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['full_name', 'phone']  # Add any additional required fields
+
     class Meta:
         db_table = 'users'
+
     def __str__(self):
         return self.email
 
 
-  
 class password_storage(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name='user_id_repos',blank=True, null=True,db_column='created_by')
