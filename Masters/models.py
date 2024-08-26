b549c7a21c8e32e71fd9c190d54f98080b03d477
ShubhamBelekar21 <Shubham.belekar@technowin.co.in>
2024-08-26 12:38:21 +0530

    dd

--- Masters/models.py
+++ Masters/models.py
@@ -84,7 +84,7 @@ class sc_employee_master(models.Model):
     employee_id =models.TextField(null=True,blank=True)
     employee_name =models.TextField(null=True,blank=True)
     mobile_no =models.TextField(null=True,blank=True)
-    current_location =models.TextField(null=True,blank=True)
+    worksite =models.TextField(null=True,blank=True)
     employment_status = models.ForeignKey(parameter_master, on_delete=models.CASCADE,related_name='parameter_data',blank=True, null=True)
     is_active =models.BooleanField(null=True,blank=True,default=True)
     created_at = models.DateTimeField(null=True,blank=True,auto_now_add=True)
